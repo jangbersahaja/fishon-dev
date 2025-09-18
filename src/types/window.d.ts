36b2c5b4ed460d01477fd1192a53567465d7ee6e
jangbersahaja <mmuter4@gmail.com>
2025-09-18 23:04:08 +0800

    Develepmont Progress Commit 2

--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -5,6 +5,7 @@ type MarkerClustererConstructor = new (opts: object) => unknown;
 declare global {
   interface Window {
     __gmapReady?: boolean;
+    google?: typeof google;
     markerClusterer?: {
       MarkerClusterer: MarkerClustererConstructor;
     };
